4cbeb78029ca364a5d3eb7b2283b387f0f4ed856
david <etoedia@gmail.com>
2021-05-01 03:26:55 +0000

    updated ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -29,15 +20,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -62,6 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+
+          // app bar icons
           Padding(
             padding: const EdgeInsets.only(top: 60, left: 15, right: 15),
             child: Row(
@@ -80,6 +64,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
+
+          // Header text - 'Hello, i'm david'
           Padding(
             padding: const EdgeInsets.only(top: 450, left: 30),
             child: RichText(
@@ -104,6 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
+
+          //sub text - Graphics designer, flutter app developer
           Padding(
             padding: const EdgeInsets.only(top: 589, left: 30),
             child: Column(
@@ -119,6 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 35,
                 ),
+
+                //Main text
                 Text(
                   'Fuelled by a passion for designing compelling products, I have deep desire to excel and continously improve in my work.',
                   style: TextStyle(
@@ -130,6 +120,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 SizedBox(
                   height: 40,
                 ),
+
+                // text with portfolio icons
                 Row(
                   children: [
                     Text(
